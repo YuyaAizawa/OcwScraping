7532379927dce75bc67ed037b2996c3aaf496061
Yuki Tsutsui <tsutsui_y@YukinoMacBook-ea.local>
2018-10-13 00:16:50 +0900

    一括スクレイピングスクリプトimportOCW.pyとテーブル再構築スクリプトrecreateOCWTable.pyの実装

---
+++ recreateOCWTable.py
@@ -0,0 +1,76 @@
+import pymysql
+
+'''
+    recreateOCWTable.py
+
+(新規にテーブルを構築する場合はdropTable()を#でコメントアウトするよう願います)
+講義情報のテーブルを構築し直します
+打ち込んですぐ行われたらやばいので，質問フェーズを設けてます
+(yを入力すると再構築が行われ，それ以外で再構築を中止します)
+(PRIMARY KEY設定を変更する場合，createTable()を少し変える必要があります)
+
+connectionとcolumnの情報はimportOCW.pyとおなじにしてね！
+'''
+
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='',
+                             db='test_ocw',
+                             charset='utf8',
+                             # Selectの結果をdictionary形式で受け取る
+                             cursorclass=pymysql.cursors.DictCursor)
+
+#データベースのカラム情報
+#科目コードをキーとして持たせたい気持ち
+column = {# TOP側 #
+          "科目コード":"LectureCode", # キー
+          "講義名":"LectureName",
+          "担当教員名":"Professor",
+          "開講元":"Department",
+          "曜日・時限(講義室)":"DateRoom",
+          "URL":"URL",
+         #"講義室":"Room",
+          "単位数":"Credit",
+          "開講クォーター":"Quater",
+          "使用言語":"Language",
+          # シラバス側 #
+          "授業計画・課題":"LecturePlan",
+          "成績評価の基準及び方法":"AssessStyle",
+          "履修の条件(知識・技能・履修済科目等)":"CourseCond",
+          # 検索用 #
+          "学院":"Gakuin"}
+
+#TABLEをつくる　データベースの構造が完全になったら不要
+def createTable(column):
+    with connection.cursor() as cursor:
+        KEY_COLUMN = "科目コード"
+        KEY_LENGTH = 10
+
+        sub_column = []
+        for k in column:
+            if column[k] == KEY_COLUMN:
+                #key処理そのいち
+                sub_column.append(column[k]+" TEXT NOT NULL")
+            else:
+                sub_column.append(column[k]+" TEXT")
+
+        sub_column.append("PRIMARY KEY({}({}))".format(column[KEY_COLUMN],KEY_LENGTH)) #key処理そのに
+        sql = "CREATE TABLE lecture({});".format(",".join(sub_column))
+        #print(sql)
+        cursor.execute(sql)
+
+#TABLEを削除する
+def dropTable():
+    with connection.cursor() as cursor:
+        sql = "DROP TABLE lecture;"
+        cursor.execute(sql)
+
+if __name__=='__main__':
+    print("本当にOCW Tableを再構築しますか？(する：y，しない：otherwise)")
+    if input()=="y":
+        dropTable()
+        createTable(column)
+        connection.commit()
+        print("OCW Tableを再構築しました")
+    else:
+        print("OCW Tableの再構築を中止しました")
